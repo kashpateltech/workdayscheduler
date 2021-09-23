5b5b6ba3241d9b17a5cae9461bc30a63c05c41e7
Kash Patel <kaushikpatel26@yahoo.com>
2021-09-24 02:16:57 +1000

    First commit

---
+++ script.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+    //displaying current day
+    $('#currentDay').text(moment().format('Do MMMM dddd hh:mm A'));
+
+    //using if, else-if, else to track each hour as per the timeblocks in the HTML doc using CSS class for colouring(present, past, future)
+    function hourlyTimeblock() {
+        //using moment to get current hour
+        var currHour = moment().hours();
+        // traversing using.each 
+        $('.time-block').each(function(){
+        //using parseint to convert string into numbers
+            var timeBlockHour = parseInt($(this).attr('id'));
+
+            if (timeBlockHour === currHour) {
+                $(this).addClass('present').children('textarea');
+                $(this).removeClass('past').children('textarea');
+            }
+            else if (timeBlockHour < currHour) {
+                $(this).addClass('past').children('textarea');          
+            }
+            else {
+                $(this).removeClass('present').children('textarea');
+                $(this).removeClass('past').children('textarea');
+                $(this).addClass('future').children('textarea');
+            }
+
+        });
+        
+    }
+    hourlyTimeblock();
+
+     setInterval(hourlyTimeblock, 1000);
+    
+    
+     //adding event listener to save button
+     $('.saveBtn').on('click', function(){
+        
+        var entry = $(this).siblings('.description').val();
+
+        var hour = $(this).parent().attr('id');
+        
+        localStorage.setItem(hour, entry);
+        
+        
+     });
+
+     $('.time-block').each(function(){
+         var saveData = $(this).attr('id');
+         $(this).children('textarea').val(localStorage.getItem(saveData));
+     });
+
+
+});
